29089c022567c0a961314b5a99b7dd5a75af0f15
Dev <downlaodsnowinx2speed.com>
2024-01-31 16:11:50 +0530

    done datatypes and operator

---
+++ datatypes_operator/scripts.js
@@ -0,0 +1,38 @@
+
+
+//Datatype
+// int
+let num = 2;
+
+//string
+
+let name = 'tufail';
+const lastname = 'khan';
+
+//boolean
+
+let isloggegIn = false
+
+//Null 
+
+let lastLoginDate = null;
+
+console.log(lastLoginDate)
+
+//object
+
+const person = {
+	//key : value
+	name:'tom',
+	age : 22,
+	typeofage : typeof(age),
+}
+
+console.log(person)
+
+
+//operators
+
+// string '1' + '1' = string
+// string + number = string
+// string * num = number
