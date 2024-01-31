c597e88834c7ecf1a1fde09e00ece945de315f90
Dev <downlaodsnowinx2speed.com>
2024-01-31 15:49:02 +0530

    new file create

---
+++ variables_in_javascripts/scripts.js
@@ -0,0 +1,26 @@
+
+// variables 
+// var 
+
+//DataType varName  = value; strongly typed lan
+//js -> loosly type language
+
+// var -> Global (scope) 
+
+var age = 20;
+var num1 = 10;
+var num2 = 21;
+console.log("i am var variable")
+console.log(age);
+console.log(num1 + num2);
+
+//let ->local (scope)
+console.log("let")
+let ag = 22;
+console.log(ag)
+
+//const -> local (scope)
+// -> not change 
+console.log("const")
+const name = "tufail"
+console.log(name)
