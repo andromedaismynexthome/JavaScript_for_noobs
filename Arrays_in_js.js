bb984116667207e9dbd8f7f06ee2710f8389d092
Dev <downlaodsnowinx2speed.com>
2024-02-03 13:17:54 +0530

    mazeh

--- Arrays_in_js.js
+++ Arrays_in_js.js
@@ -1,1 +1,24 @@
+// arrays in js
 
+// to store multiple things in one is called array
+
+const students = ['tufail','mm','kk'];
+console.log(students);
+
+//in javascript array can be homo and hetro 
+
+// homo 
+const data = [1,2,3,-1] //only inter value
+console.log(data)
+//hetro
+const d = [1,"one",'!',true];
+console.log(d)
+
+
+// we can perform operation on arrays
+// push
+d.push(1,'h')
+//pop
+d.pop(0)
+//reverse
+console.log(d.reverse())
