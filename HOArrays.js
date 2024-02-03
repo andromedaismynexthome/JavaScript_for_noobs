c815467b5b29b0b4855ffe48f1ab15d201bd9536
Dev <downlaodsnowinx2speed.com>
2024-02-03 13:32:47 +0530

    u can do many things

--- HOArrays.js
+++ HOArrays.js
@@ -1,1 +1,18 @@
+// Arrays High order functions
+
+const students = [1,2,3,4]
+
+function print(n){
+ console.log(n)
+}
+ 
+students.forEach(print);
+
+// by arrow function
+
+//students.forEach((val)=> console.log(val));
+
+// dot map 
+
+students.map((val) => console.log(val));
 
