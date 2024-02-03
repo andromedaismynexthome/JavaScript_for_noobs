cf7d43211638133871087d2122b8ca8c08a88272
Dev <downlaodsnowinx2speed.com>
2024-02-03 13:03:24 +0530

    try to use highorder function everytime

--- HighOrderFunction_and_callBack.js
+++ HighOrderFunction_and_callBack.js
@@ -1,1 +1,20 @@
 //High order function
+
+// A function that takes a function as an argument is called high order fucntion
+                //call back
+function add(a,b,cb){ //cb is call back
+  let result = a + b;
+  cb(result);//callback
+}
+//normal function
+add(2,4, function (val){
+  console.log(val);
+});
+
+
+// by arrow function
+add(3, 4, (val)=> console.log(val));
+
+
+
+
