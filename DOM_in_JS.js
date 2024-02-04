2931cb5472e944a5c27e0e7a3e0272df41da7d81
Dev <downlaodsnowinx2speed.com>
2024-02-04 13:19:58 +0530

    queary , select, get

--- DOM_in_JS.js
+++ DOM_in_JS.js
@@ -1,1 +1,33 @@
+// DOM in js
+// Document object model
 
+// query selector
+/*
+ * selectors
+ * - tag name
+ * - id #
+ * - class .
+ */
+
+const body = document.querySelector("body");
+body.innerHTML = "<h1>heelo</h1>"
+
+
+//Select element by ID
+
+const el = document.getElementById('h');
+el.innerText = "ballw";
+
+
+// get element by class name
+
+const element = document.getElementByClassName("block");
+
+
+//event listners
+
+function handk(){
+alert('hahahha');
+}
+const l = document.getElementById("click");
+l.onclick = () => console.log("i cli");
