f4bb2cac13931dd465ae15ab70dd5e3981872c48
Dev <downlaodsnowinx2speed.com>
2024-02-05 14:18:05 +0530

    async and await function fetcinh api

--- Promises.js
+++ Promises.js
@@ -1,1 +1,29 @@
+//
+// Promises in javascript
 
+// async and await
+// asynchronus task return promise
+/*
+ * prefer most this one 
+async function getData(){
+ let result = await fetch(
+    "https://jsonplaceholder.typicode.com/posts"
+ );
+	const fe = document.getElemetById("my-container");
+	console.log(await result.json());
+        for(let i = 0;i<result.length;i++){
+           fe.appendChild("<li>result[i]</li>");
+	}
+}
+
+getData()
+
+*/
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+ .then((data)=>{
+   console.log(data);
+})
+  .catch((err)=>{
+  console.log(err)
+});
