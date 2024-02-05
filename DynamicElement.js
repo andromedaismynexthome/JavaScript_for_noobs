9ae1d6a4eadfef1673c1b13b341762e6321a982e
Dev <downlaodsnowinx2speed.com>
2024-02-05 13:38:00 +0530

    created dynamic element by using createelemt function

--- DynamicElement.js
+++ DynamicElement.js
@@ -1,1 +1,10 @@
+//
+const button = document.getElementById("click");
 
+const container = document.getElementById("my-container");
+
+button.addEventListener('click', ()=>{
+    const el = document.createElement("h1");
+	el.innerText = "Tufail";
+	container.appendChild(el);
+});
