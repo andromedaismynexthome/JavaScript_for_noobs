ba362f5c27b7c25d57f1bd66cb83142dab6999f0
Dev <downlaodsnowinx2speed.com>
2024-02-06 12:14:42 +0530

    done some changes

--- Whetherapp/scripts.js
+++ Whetherapp/scripts.js
@@ -5,18 +5,23 @@ const cityname = document.getElementById("cityname") || { value : ''};
 const citytime = document.getElementById("city-time") || { value : ''};
 const cityhumidity = document.getElementById("city- humidity") || { value : ''};
 
-async function getData(city){
+// async function getData(city){
 
+//     const promise = await fetch(`https://api.weatherapi.com/v1/current.json?key=fa31add3aa76447ca5a91441240502&q=${city}&aqi=yes`)
+//     return await promise.json()
+
+// }
+
+const getData = async (city) => {
     const promise = await fetch(`https://api.weatherapi.com/v1/current.json?key=fa31add3aa76447ca5a91441240502&q=${city}&aqi=yes`)
     return await promise.json()
-
 }
 button.addEventListener('click', async () => {
     const value = input.value;
     const result = await getData(value);
     console.log(result);
     cityname.innerText = `${result.location.country}, ${result.location.name} - ${result.location.region}`
-    cityhumidity.innerText = `humidity: ${result.current.temp_c}`
+    cityhumidity.innerText = `humidity: ${result.current.humidity}`
     citytime.innerText = result.location.localtime
     
 });
