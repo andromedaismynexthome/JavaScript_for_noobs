bf6127d8f9f579ed8e04dbfabfcefb1434411439
Dev <downlaodsnowinx2speed.com>
2024-02-07 19:15:25 +0530

    done location

--- Current_Location.js
+++ Current_Location.js
@@ -1,1 +1,18 @@
 
+const button = document.getElementById("user-location");
+
+const getData = async (lon,lati) => {
+    const promise = await fetch(`https://api.weatherapi.com/v1/current.json?key=fa31add3aa76447ca5a91441240502&q=${lon},${lati}&aqi=yes`)
+    return await promise.json()
+}
+// getting user location is not instantanias we need async fucntion
+
+button.addEventListener("click", async () => {
+        const result = navigator.geolocation.getCurrentPosition(async (position)=>{
+          const my = await getData(position.coords.latitude,position.coords.longitude,);
+            console.log(my);
+        }, ()=>{
+            console.log("there is an issue")
+        });
+
+})
