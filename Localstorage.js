4b905dad4f07756a7cd83456f9bc5ecbeeebd79c
Dev <downlaodsnowinx2speed.com>
2024-02-07 18:38:19 +0530

    learn about localstorage

--- Localstorage.js
+++ Localstorage.js
@@ -1,1 +1,15 @@
 // local storage in javascript
+const button = document.getElementById("click");
+const input = document.getElementById("input-feild");
+const lname = document.getElementById("name");
+button.addEventListener('click', () => {
+    const value  = input.value;
+    console.log(value);
+    localStorage.setItem("name", value);
+    location.reload();
+});
+
+window.addEventListener("load", ()=>{
+    const username = localStorage.getItem("name");
+    lname.innerText = username;
+})
