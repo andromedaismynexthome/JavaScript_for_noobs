e6b4388cfb619a35eea6409722d693146b161b78
Dev <downlaodsnowinx2speed.com>
2024-02-10 11:13:06 +0530

    learn about how closures work

--- Closure.js
+++ Closure.js
@@ -1,1 +1,23 @@
+// this is so important for frontend developer
+// closure is a combination of functions
 
+function main(){
+    const name = 'tufai'
+    function sayMyname(){
+        console.log(name);
+    }
+    return sayMyname();
+}
+
+//application
+function makeTextsize(size){
+
+    function changeSize(){
+        myName.style.fontSize = `${size}`;
+    }
+
+    return changeSize();
+}
+
+const chnage = makeTextsize(20);
+chnage();
