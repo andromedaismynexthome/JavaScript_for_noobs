66a5e19ac30cf12e80fe4c4d6321c835edbb18ab
Dev <downlaodsnowinx2speed.com>
2024-02-10 12:54:59 +0530

    learn about how to make normal function into promise return

--- Promisification.js
+++ Promisification.js
@@ -1,1 +1,13 @@
 
+//Promisification
+function promisify(fn) {
+    return function (...args){
+        return new Promise((resolve, reject),
+        fn(...args , (error, result) => {
+            if (error) return reject(error);
+            return resolve(result); 
+        })
+    )
+    }
+} 
+//without promise
