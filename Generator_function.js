d1142fa3e09fa4af72eedd33f1b2c19026836330
Dev <downlaodsnowinx2speed.com>
2024-02-10 12:41:02 +0530

    we can create iterator custom

--- Generator_function.js
+++ Generator_function.js
@@ -1,1 +1,46 @@
+// what is iterator pattern
+//provide the customization the behavior of for...of loops
+// function makeIterator(start = 0, end = Infinity, step=1){
+//     let nextstart = start;
+//     let interationCount = 0;
+
+//     return {
+//         next() {
+//             let result;
+//             if (interationCount < end){
+//                 result = { value: nextstart, done: false }
+//                 nextstart += step;
+//                 interationCount++;
+//                 return result;
+//             }
+
+//             return {
+//                 value: interationCount, done: true
+//             }
+//         }
+//     };
+// }
+
+// const myiteration = makeIterator(1,20,1);
+// let result = myiteration.next()
+
+// while(!result.done){
+//     console.log(result.value);
+//     result = myiteration.next();
+// }
+
+// what is generator function
+function* onetoten(start , end){
+    for(let i = start; i<=end;i++){
+        yield i;
+    }
+}
+
+const number = onetoten(1,10);
+for(const i of number){
+    console.log(i);
+}
+// what is yeild keyword
+
+
 
