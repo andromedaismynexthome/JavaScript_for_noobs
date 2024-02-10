a298405b8dbc6e30d3d039e9803b2d80ce4f0ead
Dev <downlaodsnowinx2speed.com>
2024-02-10 11:46:08 +0530

    if want perform one by one task used composed function

--- composition.js
+++ composition.js
@@ -1,1 +1,16 @@
 
+function add(a,b){
+    return a+b;
+}
+
+function square(val){
+    return val * val;
+}
+
+const composeTwofunction = (fn1,fn2) => (a,b) => fn2(fn1(a,b));
+const task = composeTwofunction(add, square);
+console.log(task(2,3));
+
+//how to compose unlimited function
+
+const composeUnlimited = (...fns) => (...values) => fns.reduce((a,b) => b(a), values);
