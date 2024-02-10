9582bc4134314bea9dd5e4c7e5a4269d774c50ec
Dev <downlaodsnowinx2speed.com>
2024-02-10 12:15:57 +0530

    as soon as function is define

--- IIFE.js
+++ IIFE.js
@@ -1,1 +1,27 @@
 
+//immediately invoke function 
+
+(function add(a,b){console.log(a+b)})(2,3);
+
+(() => console.log("hi"))();
+
+const date = (() => new Date())()
+console.log(date);
+
+//bank 
+
+const atm = ((initialbalace)=> {
+    let balance = initialbalace;
+    function withdraw(amt){
+        if(amt > balance){
+            return "are u kidding";
+        } else{
+            balance -= amt;
+            return balance;
+        }
+    }
+    return { withdraw };
+} )
+
+const tufail = atm(1000);
+console.log(tufail.withdraw(100));
